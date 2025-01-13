28a9e9fa87c1c3c19179b7c5eb25dac0910c342d
tony-o <tony.odell@live.com>
2025-01-13 11:02:24 -0800

    retry7

---
+++ retry7/__init__.py
@@ -0,0 +1,38 @@
+from random import uniform
+from time import sleep
+
+
+def retry(
+    exceptions=Exception,
+    tries=-1,
+    delay=0,
+    max_delay=None,
+    backoff=1,
+    jitter=0,
+    logger=None,
+):
+    def decorator(f, *args, **kwargs):
+        _tries, _delay = tries, delay
+        while _tries:
+            try:
+                return f(*args, **kwargs)
+            except exceptions as e:
+                _tries -= 1
+                if _tries == 0:
+                    raise e
+
+                if logger is not None:
+                    logger.warning(f"{e}, retrying in {delay} seconds...")
+
+                sleep(_delay)
+                _delay *= backoff
+
+                if isinstance(jitter, tuple):
+                    _delay += uniform(*jitter)
+                else:
+                    _delay += jitter
+
+                if max_delay is not None:
+                    _delay = min(_delay, max_delay)
+
+    return decorator
---
+++ tests/test_all.py
@@ -0,0 +1,107 @@
+# from retry7 import retry
+import retry7
+
+retry = retry7.retry
+
+
+def test_base():
+    @retry()
+    def f():
+        1
+
+    assert True
+
+
+def test_exceptions_base():
+    # catches generic exceptions
+    tries = 0
+    errored = None
+    try:
+
+        @retry(tries=2)
+        def f():
+            nonlocal tries
+            tries += 1
+            42 / 0
+
+    except Exception as e:
+        errored = e
+    assert tries == 2
+    assert isinstance(errored, ZeroDivisionError)
+
+
+def test_exceptions_list_and_custom():
+    class X1(Exception):
+        pass
+
+    class X2(Exception):
+        pass
+
+    thrown = []
+
+    try:
+        to_throw = 0
+
+        @retry(exceptions=(X1, X2))
+        def f():
+            nonlocal to_throw
+            if to_throw == 0:
+                to_throw += 1
+                raise X1()
+            elif to_throw == 1:
+                to_throw += 1
+                raise X2()
+            else:
+                raise Exception()
+
+    except Exception as e:
+        thrown += [e]
+
+    assert len(thrown) == 1
+    assert not (isinstance(thrown[0], X1) or isinstance(thrown[0], X2))
+
+
+def test_delay():
+    slept = []
+
+    def mock_sleep(x):
+        nonlocal slept
+        slept += [x]
+
+    retry7.sleep = mock_sleep
+
+    try:
+
+        @retry(tries=3, delay=5)
+        def f():
+            raise Exception
+
+    except Exception:
+        pass
+
+    assert len(slept) == 2
+    assert slept[0] == 5
+    assert slept[1] == 5
+
+
+def test_backoff():
+    slept = []
+
+    def mock_sleep(x):
+        nonlocal slept
+        slept += [x]
+
+    retry7.sleep = mock_sleep
+
+    try:
+
+        @retry(tries=3, delay=5, backoff=2)
+        def f():
+            raise Exception
+
+    except Exception:
+        pass
+
+    assert len(slept) == 2
+    assert slept[0] == 5
+    assert slept[1] == 10
